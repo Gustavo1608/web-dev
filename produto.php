fdcd3d520f2334ec36f86460bfd05da359000642
Gustavo1608 <gustavomilbeyer@gmail.com>
2016-11-07 16:08:57 -0200

    lição dia 07/11
    lição de sql do dia 07/11

--- produto.php
+++ produto.php
@@ -1,21 +1,26 @@
 <?php
-
 $link = mysqli_connect('localhost','root','');
 if (!$link) {
     die('Não foi possível conectar: ' . mysql_error());
 }
-
 $produto_id = $_GET['p'];
 
-mysqli_select_db($link, 'chavao');
+mysqli_select_db($link, 'nossa jureg');
 
-$resultado = mysqli_query($link, "SELECT * FROM produtos WHERE id = $produto_id");
+$resultado = mysqli_query($link, "SELECT * FROM produtos WHERE id= $produto_id");
 
+$comentarios = mysqli_query($link, "SELECT * FROM comentarios WHERE id_produto = $produto_id");
+    
 while ($linha = mysqli_fetch_assoc($resultado) ) {
-    echo "<h1>$linha[nome]</h1>";
+   echo "<h1>$linha[nome]</h1>";
     echo "<h2>$linha[valor]</h2>";
     echo "<small>$linha[qnt] unidade disponíveis</small>";
     echo "<p>$linha[descricao]</p>";
-}
 
+    while ($coment = mysqli_fetch_assoc($comentarios)) {
+        echo "<p>$coment[comentario]</p>";
+        echo "<p><b>Nota: $coment[nota]</b></p>";
+    }
+}
+    
 ?>
